994162689b27b12e54fd6d5f3b8988cd329170f2
Steve <stevedsimkins@gmail.com>
2024-12-15 08:00:31 -0500

    chore: Updated polling and max requests

--- ponder.config.ts
+++ ponder.config.ts
@@ -4,19 +4,20 @@ import { http } from "viem";
 import { ConcealmintAbi } from "./abis/ConcealmintAbi";
 
 export default createConfig({
-  networks: {
-    baseSepolia: {
-      chainId: 84532,
-      transport: http(process.env.PONDER_RPC_URL_84532),
-    },
-  },
-  contracts: {
-    Concealmint: {
-      abi: ConcealmintAbi,
-      address: "0x9B046Ca68dD4c7F7fC36A98DEdecf01C8EE904f7",
-      network: "baseSepolia",
-      startBlock: 19169268,
-      interval: 60 / 12
-    },
-  },
+	networks: {
+		baseSepolia: {
+			chainId: 84532,
+			transport: http(process.env.PONDER_RPC_URL_84532),
+			pollingInterval: 2_000,
+			maxRequestsPerSecond: 100,
+		},
+	},
+	contracts: {
+		Concealmint: {
+			abi: ConcealmintAbi,
+			address: "0x9B046Ca68dD4c7F7fC36A98DEdecf01C8EE904f7",
+			network: "baseSepolia",
+			startBlock: 19169268,
+		},
+	},
 });
